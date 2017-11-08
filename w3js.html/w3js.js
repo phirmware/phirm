9612cf2fe5a1d0c7d660b3b6108d8a620782548d
nerdeveloer <odirionye@gmail.com>
2017-11-08 08:35:18 +0100

    uploaded new stuff

---
+++ w3js.html/w3js.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $("button").click(function(){
+        $("#modal").show();
+    })
+});
+
+$(document).ready(function(){
+    $(".clsbtn").click(function(){
+        $("#modal").hide();
+    })
+});
